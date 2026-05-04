34e0219f784b04b2ba9dafb2cd7f6ff221c3b4c2
gebercorparation-hash <geber.corparation@gmail.com>
2026-05-04 20:36:15 +0100

    deploy: auto build and push

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-// Service Worker v20260504-v5 — Web Push + llamadas + mensajes + auto-renovación + caché offline + force-update
-const CACHE = 'egchat-v20260504-nodebug';
+// Service Worker v20260504-v4 — Web Push + llamadas + mensajes + auto-renovación + caché offline + force-update
+const CACHE = 'egchat-v20260504-morlmfjo';
 const API_BASE = 'https://egchat-api.onrender.com';
 const VAPID_PUBLIC_KEY = 'BNeDJFYqIX59vgqEKxWfrI263knyPGHafMEK_WrMPeYaIm8bn62vcOah7hDlgIek4R4utB82g-cT9CwAtGn0wUs';
 
