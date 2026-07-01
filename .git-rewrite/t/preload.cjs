ffac8fda76989d20c08d2adffdf2e0b270e15f76
gebercorparation-hash <geber.corparation@gmail.com>
2026-07-01 13:43:21 +0100

    deploy: auto build and push

--- .git-rewrite/t/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
-  openExternalUrl: (url) => ipcRenderer.invoke('open-external-url', url),
-});
--- .git-rewrite/t/server.js
+++
@@ -1,4 +0,0 @@
-import('./index.cjs').catch((err) => {
-  console.error('Failed to start EGCHAT server:', err);
-  process.exit(1);
-});
