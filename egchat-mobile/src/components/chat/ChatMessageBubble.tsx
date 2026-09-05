430cecdc6029ba651b5dafe66b868b9181acd52b
RAYMON REDDINTONE <raymonreddintone@RAYMONs-MacBook-Pro.local>
2026-09-05 13:08:05 +0100

    auto: cambios guardados por Kiro

--- egchat-mobile/src/components/chat/ChatMessageBubble.tsx
+++ egchat-mobile/src/components/chat/ChatMessageBubble.tsx
@@ -14,6 +14,7 @@ import { ReactionBubble, ReactionPopAnimation } from './ReactionBubble';
 import { PollMessage, parsePoll } from './PollMessage';
 import { ImageViewer } from '../ImageViewer';
 import { LinkPreview, extractUrl } from './LinkPreview';
+import { VideoViewerModal } from './VideoViewerModal';
 import { MarkdownText } from './MarkdownText';
 import type { ChatMessage } from '../../types/chat';
 
