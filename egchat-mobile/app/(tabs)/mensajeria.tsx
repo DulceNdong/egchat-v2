6fb86b064daf87675105383250233a4595de4e16
RAYMON REDDINTONE <raymonreddintone@RAYMONs-MacBook-Pro.local>
2026-09-06 18:27:02 +0100

    auto: cambios guardados por Kiro

--- egchat-mobile/app/(tabs)/mensajeria.tsx
+++ egchat-mobile/app/(tabs)/mensajeria.tsx
@@ -44,6 +44,7 @@ import { EGAvatar, OfflineBanner } from '../../src/components/ui';
 import { NotificationsPanel, HamburgerMenu, WeatherModal, AppNotification } from '../../src/components/HeaderPanels';
 import { EGChatHeader } from '../../src/components/EGChatHeader';
 import { SpinningLogo } from '../../src/components/SpinningLogo';
+import { DraggableHomeButton } from '../../src/components/DraggableHomeButton';
 import {
   Colors, Typography, Spacing, BorderRadius,
   FontSize, FontWeight, Shadow,
